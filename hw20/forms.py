f995a327d16c44019b0c3205f5dce9160583ab20
valid <valid.saley@gmail.com>
2020-01-16 19:15:53 +0300

    fix

--- hw20/forms.py
+++ hw20/forms.py
@@ -7,13 +7,13 @@ class OrderForm(forms.Form):
     from_field = forms.CharField(max_length=50)
     to_field = forms.CharField(max_length=50)
     count_of_people = forms.IntegerField()
-    date = forms.DateField(input_formats=['%Y-%m-%d'])
+    date = forms.DateField(widget=forms.SelectDateWidget, input_formats=['%Y-%m-%d'])
 
     name.widget.attrs.update({'class': 'form-control col-3'})
     from_field.widget.attrs.update({'class': 'form-control col-3'})
     to_field.widget.attrs.update({'class': 'form-control col-3'})
     count_of_people.widget.attrs.update({'class': 'form-control col-3'})
-    date.widget.attrs.update({'class': 'form-control col-3'})
+
 
     def clean_name(self):
         name = self.cleaned_data['name']
