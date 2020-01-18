bc75d2780d49b0bfbcccf674b09a6f94e8c50cde
valid <valid.saley@gmail.com>
2020-01-18 18:20:06 +0300

    user _create _delete

--- hw21/urls.py
+++ hw21/urls.py
@@ -3,6 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', user_list, name='user_list_url'),
+    path('user/create', user_create, name='user_create_url'),
     path('user/<int:id>', user_detail, name='user_detail_url'),
     path('user/<int:id>/edit', user_edit, name='user_edit_url'),
+    path('user/<int:id>/delete', user_delete, name='user_delete_url'),
 ]
--- hw21/views.py
+++ hw21/views.py
@@ -14,6 +14,20 @@ def user_detail(request, id):
     return render(request, 'hw21/user_detail.html', context={'user': user})
 
 
+def user_create(request):
+    if request.method == 'GET':
+        form = UserForm()
+        return render(request, 'hw21/user_create.html', context={'form': form})
+
+    if request.method == 'POST':
+        form = UserForm(request.POST)
+
+        if form.is_valid():
+            new_user = form.save()
+            return redirect('user_detail_url', id=new_user.id)
+        return render(request, 'hw21/user_create.html', context={'form': form})
+
+
 def user_edit(request, id):
     if request.method == 'GET':
         user = User.objects.get(id=id)
@@ -27,3 +41,13 @@ def user_edit(request, id):
             updated_user = form.save()
             return redirect('user_detail_url', id=updated_user.id)
         return render(request, 'hw21/user_edit.html', context={'form': form, 'user': user})
+
+
+def user_delete(request, id):
+    if request.method == 'GET':
+        user = User.objects.get(id=id)
+        return render(request, 'hw21/user_delete.html', context={'user': user})
+
+    if request.method == 'POST':
+        User.objects.get(id=id).delete()
+        return redirect('user_list_url')
