94b835fe068dc32c34db020c7da35fcd7a5da4a3
valid <valid.saley@gmail.com>
2020-01-27 17:35:07 +0300

    hw23 decorator crud

--- hw23/urls.py
+++ hw23/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import api_car
+from .views import api_car, api_car_detail
+
 
 urlpatterns = [
-    path('car/', api_car)
+    path('car/', api_car, name='api_car_url'),
+    path('car/<int:pk>', api_car_detail),
 ]
--- hw23/views.py
+++ hw23/views.py
@@ -1,14 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, reverse, redirect
 from .serializers import CarSerializer
 from .models import Car
+from django.urls import reverse_lazy
 from django.http import JsonResponse
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST', 'DELETE'])
 def api_car(request):
-    if request.method == 'GET':
+    if request.method == 'GET' or request.method == 'DELETE':
         cars = Car.objects.all()
         serializer = CarSerializer(cars, many=True)
         return Response(serializer.data)
+    if request.method == 'POST':
+        serializer = CarSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def api_car_detail(request, pk):
+    car = Car.objects.get(id=pk)
+    if request.method == 'GET':
+        serializer = CarSerializer(car)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CarSerializer(car, request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == 'DELETE':
+        car.delete()
+        return redirect(reverse_lazy(api_car))
